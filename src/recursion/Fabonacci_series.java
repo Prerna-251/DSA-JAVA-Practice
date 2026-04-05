8f54e714f216708db2bb0e6f6406cffa0e654cf3
Prerna-251 <prernagayakwad2020@gmail.com>
2026-04-05 21:27:48 +0530

    Fabonacci series using recursion

--- src/recursion/Fabonacci_series.java
+++ src/recursion/Fabonacci_series.java
@@ -3,29 +3,19 @@ package recursion;
 import java.util.Scanner;
 
 public class Fabonacci_series {
-	void fabonacci(int num) {
-		int n0 = 0, n1 = 1;
-		if(num <= 0) {
-			return;
+	 int fabonacci(int num) {
+		if(num <= 1) {
+			return num;
 		}
-		if( num == 1) {
-			System.out.print(n0);
-			return;
-		}
-		System.out.print(n0+" "+n1);
-		for(int i=2; i<num; i++) {
-			int n2 = n0 + n1;
-			System.out.print(" "+n2);
-			n0 = n1; 
-			n1 = n2;
-		}
+		return fabonacci(num-1) + fabonacci(num-2);
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		Fabonacci_series obj = new Fabonacci_series();
-		obj.fabonacci(num);
+		for(int i=0; i<num; i++) {
+			System.out.print(obj.fabonacci(i)+" ");
+		}
 	}
-
 }
