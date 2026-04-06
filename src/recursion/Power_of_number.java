4d1fb8be6ebe4a48352b524d43f7c47a92b9ab8d
Prerna-251 <prernagayakwad2020@gmail.com>
2026-04-06 21:57:45 +0530

    Power of number

---
+++ src/recursion/Power_of_number.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class Power_of_number {
+	static int power(int x, int n) {
+		if(n == 0) {
+			return 1;
+		}
+		return x*power(x, n-1);
+	}
+	
+	public static void main(String[] args) {
+		int x = 2;
+		int n = 3;
+		System.out.print(power(x, n));
+	}
+
+}
