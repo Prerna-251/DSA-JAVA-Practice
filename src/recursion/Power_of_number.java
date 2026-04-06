9ed60b9c32087b1c4e4e57ff7b6c104ba80b518c
Prerna-251 <prernagayakwad2020@gmail.com>
2026-04-06 22:01:45 +0530

    Power of number user input

--- src/recursion/Power_of_number.java
+++ src/recursion/Power_of_number.java
@@ -1,17 +1,31 @@
 package recursion;
 
-public class Power_of_number {
-	static int power(int x, int n) {
-		if(n == 0) {
-			return 1;
-		}
-		return x*power(x, n-1);
-	}
-	
-	public static void main(String[] args) {
-		int x = 2;
-		int n = 3;
-		System.out.print(power(x, n));
-	}
+import java.util.Scanner;
 
+public class Power_of_number {
+    
+	static double power(int x, int n) {
+	    if (n == 0) return 1;
+
+	    if (n < 0) {
+	        return 1.0 / power(x, -n);
+	    }
+
+	    return x * power(x, n - 1);
+	}
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter base (x): ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter power (n): ");
+        int n = sc.nextInt();
+
+        double result = power(x, n);
+        System.out.println("Result: " + result);
+
+        sc.close();
+    }
 }
