a8a76c109c945ce37a0752d8e4b31272b7db299f
Prerna-251 <prernagayakwad2020@gmail.com>
2026-04-08 23:46:59 +0530

    Find duplicate element

---
+++ src/hashing/Dumplicate_element.java
@@ -0,0 +1,22 @@
+package hashing;
+
+import java.util.*;
+public class Dumplicate_element {
+
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 2, 4, 1};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int key : map.keySet()) {
+            if (map.get(key) > 1) {
+                System.out.println(key);
+            }
+        }
+	}
+
+}
