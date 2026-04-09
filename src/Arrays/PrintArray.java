40975176e2c63cef41b1cde66dd3da48ff864bb8
Prerna-251 <prernagayakwad2020@gmail.com>
2026-04-09 22:22:38 +0530

    Print User input  Array

--- src/Arrays/PrintArray.java
+++ src/Arrays/PrintArray.java
@@ -1,9 +1,18 @@
 package Arrays;
 
+import java.util.Scanner;
 public class PrintArray {
 
 	public static void main(String[] args) {
-		int arr[] = {1, 2, 3, 4, 5};
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int arr[] = new int[a];
+		
+		for(int i=0; i<a; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.print("Array is : ");
+		
 		for(int i=0 ; i<arr.length; i++) {
 			System.out.print(arr[i]+" ");
 		}
