d6e03a41b9a65f6b94aec554f15092f2991c3c36
Prerna-251 <prernagayakwad2020@gmail.com>
2026-04-22 23:34:54 +0530

    SelectionSort

--- src/sorting/Selection_sort.java
+++ src/sorting/Selection_sort.java
@@ -3,25 +3,6 @@ package sorting;
 import java.util.Scanner;
 
 public class Selection_sort {
-	
-	public static void selectionSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
-
-            // swap
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
-    }
 
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
@@ -30,8 +11,21 @@ public class Selection_sort {
 		 for(int i=0; i<a; i++) {
 			 arr[i] = sc .nextInt();
 		 }
-	        selectionSort(arr);
+		 int n = arr.length;
 
+	        for (int i = 0; i < n - 1; i++) {
+	            int minIndex = i;
+
+	            for (int j = i + 1; j < n; j++) {
+	                if (arr[j] < arr[minIndex]) {
+	                    minIndex = j;
+	                }
+	            }
+	            // swap
+	            int temp = arr[i];
+	            arr[i] = arr[minIndex];
+	            arr[minIndex] = temp;
+	        }  
 	        for (int num : arr) {
 	            System.out.print(num + " ");
 	        }
