9957183e46c3039e2bb1439bc4e86d2ee2cd743e
Prerna-251 <prernagayakwad2020@gmail.com>
2026-04-25 18:17:54 +0530

    Bubble Sort

--- src/sorting/Bubble_sort.java
+++ src/sorting/Bubble_sort.java
@@ -14,20 +14,14 @@ public class Bubble_sort {
 		 }
 		 int n = arr.length;
 
-	        for (int i = 0; i < n - 1; i++) {
-	            boolean swapped = false;
-
-	            for (int j = 0; j < n - 1 - i; j++) {
-	                if (arr[j] > arr[j + 1]) {
-	                   
-	                    int temp = arr[j];
-	                    arr[j] = arr[j + 1];
-	                    arr[j + 1] = temp;
-	                    swapped = true;
-	                }
-	            }
-
-	            if (!swapped) break;
+	        for(int i=n-1; i>=0; i--) {
+	        	for(int j=0; j<n-1; j++) {
+	        		if(arr[j]>arr[j+1]) {
+	        			int temp = arr[j+1];
+	        			arr[j+1] = arr[j];
+	        			arr[j] = temp;
+	        		}
+	        	}
 	        }
 
 	        for (int num : arr) {
